1e940695fc6ab73e4422c32a052153ef07717595
Mun JunHyeok <jjhh0919@gmail.com>
2022-11-06 16:11:52 +0900

    Create BottomTabNavigation

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import BottomTabNavigation from "./src/components/BottomTabNavigation";
+import HomeScreen from "./src/Screens/HomeScreen";
+import ManagementScreen from "./src/Screens/ManagementScreen";
+import CalendarScreen from "./src/Screens/CalendarScreen";
+import ListScreen from "./src/Screens/ListScreen";
 
-export default function App() {
-  const name = "antudy";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <BottomTabNavigation
+        screen1={HomeScreen}
+        screen2={ManagementScreen}
+        screen3={CalendarScreen}
+        screen4={ListScreen}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
+
+export default App;
---
+++ src/Screens/ManagementScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import * as React from "react";
+
+const ManagementScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Management!</Text>
+    </View>
+  );
+};
+
+export default ManagementScreen;
