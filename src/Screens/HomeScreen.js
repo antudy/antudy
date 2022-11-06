3d9b5d4ec721581ed37360ddaa80d4d8306ae890
Mun JunHyeok <jjhh0919@gmail.com>
2022-11-06 16:48:31 +0900

    Add Swiper

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import TopBar from "../components/TopBar";
 import FloatingActionButton from "../components/FloatingActionButton";
+import HomeSwiper from "../components/HomeSwiper";
 
 function HomeScreen() {
   return (
     <>
       <TopBar title={"딜리언즈"} />
-      <FloatingActionButton />
+      <View style={styles.container}>
+        <View style={styles.swiper}>
+          <HomeSwiper />
+        </View>
+        <FloatingActionButton />
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  swiper: { flex: 0.8 },
+});
 
 export default HomeScreen;
