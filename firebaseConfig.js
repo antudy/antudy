a9a525c6b654ea9bb12de2510debbaf3a6f3187d
hy219 <jhy_0219@daum.net>
2022-11-22 17:51:59 +0900

    스터디 생성창에 기본이미지 삽입 완료_[해결x]image.js에서 토큰 없이 접근이 가능하도록 해야함.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,20 +4,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import config from './firebase.json';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDtgotFirQ0TCaSZvFFFJf1apMq7vblkNs",
-  authDomain: "antudy-66cb6.firebaseapp.com",
-  projectId: "antudy-66cb6",
-  storageBucket: "antudy-66cb6.appspot.com",
-  messagingSenderId: "575659192517",
-  appId: "1:575659192517:web:e8890618cc2b65b8b700ac",
-  measurementId: "G-J4GZM650FX",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDtgotFirQ0TCaSZvFFFJf1apMq7vblkNs",
+//   authDomain: "antudy-66cb6.firebaseapp.com",
+//   projectId: "antudy-66cb6",
+//   storageBucket: "antudy-66cb6.appspot.com",
+//   messagingSenderId: "575659192517",
+//   appId: "1:575659192517:web:e8890618cc2b65b8b700ac",
+//   measurementId: "G-J4GZM650FX",
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
 // const analytics = getAnalytics(app);
 export const db = getFirestore(app);
---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+import { Firestore } from "firebase/firestore";
+const prefix = 'https://firebasestorage.googleapis.com/v0/b/antudy-66cb6.appspot.com/o'
+// const prefix = 'https://console.firebase.google.com/project/antudy-66cb6/storage/antudy-66cb6.appspot.com/files'
+
+export const images = {
+    // logo: `${prefix}/logo.png?alt=media`,
+    photo: `${prefix}/photo.png?alt=media&token=2eea3929-ab8e-412e-a2dc-48972388bb86`
+};
