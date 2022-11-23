f3ef3bedf52449c9ff83582178cf68f75f791934
hy219 <jhy_0219@daum.net>
2022-11-23 20:38:45 +0900

    스터디 생성 정보 저장 완료_이미지 업로드x

--- src/Screens/CreateStudyScreen.js
+++ src/Screens/CreateStudyScreen.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import { query, collection, getDocs, where, doc, setDoc, addDoc } from 'firebase/firestore';
+import { db } from "../../firebaseConfig";
+// import * as SecureStore from 'expo-secure-store';
+// import firestore from '@react-native-firebase/firestore';
+import React, { useEffect, useState } from "react";
 //drop down
 import DropDownPicker from "react-native-dropdown-picker";
 //image upload
@@ -69,8 +73,30 @@ const CreateStudyScreen = ({ navigation }) => {
   //image 추가
   const [photoUrl, setPhotoUrl] = useState(images.photo);
 
+  //새로운 스터디 등록하기 버튼 클릭 시
   const pressCreateStudyButton = () => {
     console.log("Press Button");
+    //스터디 생성 정보 DB 저장
+    const ANTUDYRef = query(collection(db,"ANTUDY"));
+    addDoc(ANTUDYRef, { 
+      adminTitle: `${text}`,
+      adminLocation: `${valueLocation}`,
+      adminPeople: `${valuePeople}`,
+      adminCategory: `${valueCategory}`,
+      adminDescription: `${text2}`
+      // adminTitle: "test3",
+      // adminLocation: "test3",
+      // adminPeople: "test3",
+      // adminCategory: "test3" 
+      })
+    .then(()=>{
+      console.log("Document successfully written!", ANTUDYRef.id);
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    })
+
+    //화면 전환
     navigation.navigate("Management");
   };
 
