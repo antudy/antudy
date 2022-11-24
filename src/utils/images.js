f74210ea282b4dbda1d1e3e9c835269cc21ee19f
허재혁 <gjwogur0325@naver.com>
2022-11-24 16:46:10 +0900

    배경화면, 로딩화면 수정

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,7 +2,10 @@ import { Firestore } from "firebase/firestore";
 const prefix = 'https://firebasestorage.googleapis.com/v0/b/antudy-66cb6.appspot.com/o'
 // const prefix = 'https://console.firebase.google.com/project/antudy-66cb6/storage/antudy-66cb6.appspot.com/files'
 
+import Background from '../../assets/background.jpg';
+
 export const images = {
     // logo: `${prefix}/logo.png?alt=media`,
-    photo: `${prefix}/photo.png?alt=media&token=2eea3929-ab8e-412e-a2dc-48972388bb86`
+    photo: `${prefix}/photo.png?alt=media&token=2eea3929-ab8e-412e-a2dc-48972388bb86`,
+    background: Background
 };
