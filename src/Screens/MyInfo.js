0a819c6d2ca98a188a55fc55bd466ec88cfa2a4b
허재혁 <gjwogur0325@naver.com>
2022-11-28 15:36:32 +0900

    MyInfo.js 디자인 수정

--- src/Screens/MyInfo.js
+++ src/Screens/MyInfo.js
@@ -26,59 +26,55 @@ const MyInfo = ({ navigation }) => {
         <KeyboardAwareScrollView
         contentContainerStyle={{ flex: 1 }}
         extraScrollHeight={20}
-      >
+        >
             <View style={styles.container}>
-                <Text style={styles.Text}>
-                    내정보                  <TouchableOpacity style={{backgroundColor: 'red',
-                    padding:7,
-                    margin:20,
-                    borderRaduis: 8,}}
-                        onPress={() => alert('click')}>
-                            <Text style={{color: 'white', fotSize: 12}}>Sign out</Text>
+                <View style={styles.MyInfoTitle}>
+                    <Text style={styles.Text}>
+                        내정보
+                    </Text>                  
+                    <TouchableOpacity style={styles.Signout}
+                    onPress={_handleLogoutButtonPress}>
+                    <Text style={{color: 'white', fontSize: 18}}>Sign out</Text>
                     </TouchableOpacity>
-                </Text>
+                </View>
                     <TextInput style={styles.InfoBox}
                         multiline
                         onChangeText={onChangeText}
                         value={text}
+                        underlineColor="transparent"
+                        theme={{ roundness: 0 }} 
                     />
                 <View style={styles.IngBox}>
                     <TouchableOpacity style={{backgroundColor:'#FFFFFF',padding:20,margin:12,borderRadius:8,}}
                     onPress={() => alert('연결!')}
                     >
                         <Text style={styles.buttonText}>참여중인 스터디 </Text>
-                        </TouchableOpacity>
-                            <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
-                        <TouchableOpacity style={{backgroundColor:'#FFFFFF',padding:20,margin:12,borderRadius:8}}
-                        onPress={() => alert('연결!')}
-                        >   
-                            <Text style={styles.buttonText}>일정 캘린더 </Text>
-                        </TouchableOpacity>
-                            <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
-                        <TouchableOpacity style={{backgroundColor:'#FFFFFF',padding:20,margin:12,borderRadius:8,}}
-                        onPress={() => alert('연결!')}
-                        >
-                            <Text style={styles.buttonText}>승인대기중인 스터디 </Text>
-                        </TouchableOpacity>
-                            <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
-                    </View>
+                    </TouchableOpacity>
+                        <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
+                    <TouchableOpacity style={{backgroundColor:'#FFFFFF',padding:20,margin:12,borderRadius:8}}
+                    onPress={() => alert('연결!')}
+                    >   
+                        <Text style={styles.buttonText}>일정 캘린더 </Text>
+                    </TouchableOpacity>
+                        <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
+                    <TouchableOpacity style={{backgroundColor:'#FFFFFF',padding:20,margin:12,borderRadius:8,}}
+                    onPress={() => alert('연결!')}
+                    >
+                        <Text style={styles.buttonText}>승인대기중인 스터디 </Text>
+                    </TouchableOpacity>
+                        <View style={{borderBottomColor:'#000000',borderBottomWidth:1}}></View>
                 </View>
+            </View>
         </KeyboardAwareScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
-        alignItems:'center',
-        justifyContent:'center',
+        
         backgroundColor: '#FFFFFF',
         flex:1,
-        padding: 15,
-    },
-    MyinfoTitle:{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        margin: 10,
+        padding: 20,
     },
     MyInfoTitle:{
         marginTop: 15,
@@ -93,24 +89,18 @@ const styles = StyleSheet.create({
     },
     InfoBox:{
         backgroundColor:"#FF8730",
-        width:'90%',
+        width:'100%',
         flex:1,
-        
-        
     },
     IngBox:{
         backgroundColor:"#B2B2B2",
-        width:'90%',
+        width:'100%',
         marginTop:10,
         marginBottom:30,
         flex:2,
     },
     Text:{
         fontSize:36,
-        alignSelf:'flex-start',
-        marginTop:40,
-        marginLeft:30,
-        marginBottom:10,
     },
     buttonText:{
         Color:"black",
